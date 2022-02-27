fad9a42fc445a88e8877d17c495d86589037e2dd
Mohannedkandil <mohanadmkandil@gmail.com>
2022-02-27 22:28:20 +0200

    feat(input): Add Input in the middle section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import Feed from './Feed';
 import Header from './Header'
 import Sidebar from './Sidebar'
 function App() {
@@ -9,10 +10,9 @@ function App() {
       <Header />
       <div className="app__body">
         <Sidebar />
+        <Feed />
       </div>
-      {/* App Body */}
-      {/* Sidebar */}
-      {/* Feed */}
+
       {/* Widgets */}
     </div>
   );
